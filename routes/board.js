7d04b8feebb97b4e141cbeb90ad2aa231513dba1
root <root@localhost.localdomain>
2018-04-05 10:20:35 -0400

    게시글 삭제 기능 추가

--- routes/board.js
+++ routes/board.js
@@ -5,10 +5,11 @@ var router = express.Router();
 var session = require('express-session');
 const url = require('url');
 
+// 게시글 리스트 보기
 router.route('/list').get(function(req, res){
 	
 	var con = mysql.createConnection(dbconfig);
-	var sql = "SELECT bno, title, writer, regdate, viewcnt FROM tbl_board ORDER BY bno DESC";
+	var sql = "SELECT bno, title, nickname, regdate, viewcnt FROM tbl_board ORDER BY bno DESC";
 	con.query(sql, function(err, result, rows, fields){
 		if(err)throw err;
 
@@ -16,6 +17,7 @@ router.route('/list').get(function(req, res){
 	});
 });
 
+// 게시글 등록
 router.route('/write')
 	.get(function(req, res){ // 폼
 		res.render('write.html',{session:req.session});
@@ -24,23 +26,63 @@ router.route('/write')
 
 		title = req.body.title;
 		content = req.body.content;
-		writer = req.body.writer;
+		writer = req.session.userId
+		nickname = req.session.nickname;
 
 		var con = mysql.createConnection(dbconfig);
-		var sql =	"INSERT INTO tbl_board(title, writer, content, regdate)"
-		sql +=		"	VALUES('" + title + "', '" + writer + "', '" + content + "', NOW())"
+		var sql =	"INSERT INTO tbl_board(title, writer, nickname, content, regdate)"
+		sql +=		"	VALUES('" + title + "', '" + writer + "', '" + nickname + "', '" + content + "', NOW())"
 		con.query(sql, function(err, result){
 			if(err)throw err;
 
 			console.log(result);
-			msg="성공적으로 글을 게시하였습니다";
-			res.redirect(url.format({
-				pathname:'/'
-				,query:{
-					msg:msg
-				}
-			}));
+
+			// 게시완료 alert 해야함
+			res.redirect('/');
 		});
 	});
+
+// 게시글 상세 읽기
+router.route('/read').get(function(req, res){
+	bno = req.query.bno;
+
+	var con = mysql.createConnection(dbconfig);
+	var sql =	"SELECT bno, title, writer, nickname, content "
+	sql +=		"FROM tbl_board ";
+	sql +=		"WHERE bno = '" + bno + "' ";
+	con.query(sql, function(err, result){
+		if(err)throw err;
+		console.log(result);
+
+		title = result[0].title;
+		writer = result[0].writer;
+		nickname = result[0].nickname;
+		content = result[0].content;
+
+		res.render('read.html',{
+			session: req.session
+			,bno: bno
+			,title: title
+			,writer: writer
+			,nickname: nickname
+			,content: content
+		});
+	});
+});
+
+// 게시글 삭제
+router.route('/delete').delete(function(req, res){
+	bno = req.body.bno;
+	var con = mysql.createConnection(dbconfig);
+	var sql =	"DELETE FROM tbl_board "
+	sql +=		"WHERE bno = '" + bno + "'";
+	con.query(sql, function(err, result){
+		if(err)throw err;
+		console.log(result);
+
+		// 삭제 완료 alert 해야함
+		res.redirect('/');
+	});
+});
 
 module.exports = router;
