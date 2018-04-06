c2c9367a8e86a9fabf8932386f2b974e4d5304b7
root <root@localhost.localdomain>
2018-04-06 07:33:26 -0400

    페이징 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,54 @@ var router = express.Router();
 var session = require('express-session');
 
 router.route('/').all(function(req, res){
-	/*
+	page  = req.query.page;
+	if(page == null || page == "" || page <= 0) page=1;
+	else page*=1;
+	
+	pageSize = req.query.pageSize;
+	if(pageSize == null || pageSize == "" || pageSize <= 0) pageSize=15;
+	else pageSize*=1;
+
+	var param = {};
+	param.page = page;
+	param.pageSize = pageSize;
+
 	var con = mysql.createConnection(dbconfig);
-	var sql = 'SELECT * FROM tbl_login';	
-	con.query(sql, function(err, result, fields){
+	var sql = "SELECT COUNT(*) as cnt FROM tbl_board";
+	con.query(sql, function(err, result){
 		if(err)throw err;
-
 		console.log(result);
-		res.render("index.html",{
-			title: "MY HOMEPAGE"
-			,result: result
-		});
-		con.end();
-	});
-	*/
+		var cnt = result[0].cnt;
+		
+		if(cnt <= 0){
+			res.render('index.html',{
+				cnt: cnt
+				,param:param
+				,session: req.session
+				,msg:''
+			});
+			return false;
+		}
 
-	res.render("index.html",{
-		session: req.session
-		,msg : ''
+		sql =	" SELECT bno, title, nickname, regdate, viewcnt";
+		sql +=	" FROM tbl_board";
+		sql +=	" ORDER BY bno DESC";
+		sql +=	" LIMIT " + ((page-1)*pageSize) + ", " + pageSize;
+		//sql +=	" ORDER BY bno DESC";
+		con.query(sql, function(err, result){
+			if(err)throw err;
+			console.log(result);
+			console.log(param);
+			
+			res.render('index.html',{
+				cnt:cnt
+				,param:param
+				,result: result
+				,session: req.session
+				,msg:''
+			});
+		});
+
 	});
 });
 
@@ -42,13 +72,22 @@ router.route('/login').post(function(req, res){
 
 		if(nickname == null || nickname == ""){
 			console.log('로그인 실패');
+			res.redirect('/');
+			return false;
 		}
-		else{
-			console.log('로그인 성공');
-			req.session.userId = req.body.id;
-			req.session.nickname = nickname;
-		}
-		res.redirect('./');
+	
+		console.log('로그인 성공');
+		req.session.userId = req.body.id;
+		req.session.nickname = nickname;
+	
+		sql = "UPDATE tbl_login SET last_login = NOW() WHERE id = '" + req.body.id + "'";
+		con.query(sql, function(err, result){
+			if(err)throw err;
+			console.log(result.message);
+
+			res.redirect('/');
+		});
+	
 	});
 });
 
