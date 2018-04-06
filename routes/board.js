32b1e37339b3d94f396e73e9c5c0dfd7d674e634
root <root@localhost.localdomain>
2018-04-06 03:55:59 -0400

    회원가입 기능 추가

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,6 @@ var mysql = require('mysql');
 var dbconfig = require('./config/database.js');
 var router = express.Router();
 var session = require('express-session');
-const url = require('url');
 
 // 게시글 리스트 보기
 router.route('/list').get(function(req, res){
@@ -35,9 +34,8 @@ router.route('/write')
 		con.query(sql, function(err, result){
 			if(err)throw err;
 
-			console.log(result);
+			console.log(result.message);
 
-			// 게시완료 alert 해야함
 			req.flash('msg', '성공적으로 글을 게시하였습니다');
 			res.redirect('/');
 		});
@@ -93,25 +91,34 @@ router.route('/read').get(function(req, res){
 	bno = req.query.bno;
 
 	var con = mysql.createConnection(dbconfig);
-	var sql =	"SELECT bno, title, writer, nickname, content "
-	sql +=		"FROM tbl_board ";
-	sql +=		"WHERE bno = '" + bno + "' ";
+
+	// 조회 수 증가
+	var sql = "UPDATE tbl_board SET viewcnt = viewcnt + 1 WHERE bno = '" + bno + "'";
 	con.query(sql, function(err, result){
 		if(err)throw err;
-		console.log(result);
+		console.log(result.message);
 
-		title = result[0].title;
-		writer = result[0].writer;
-		nickname = result[0].nickname;
-		content = result[0].content;
-
-		res.render('read.html',{
-			session: req.session
-			,bno: bno
-			,title: title
-			,writer: writer
-			,nickname: nickname
-			,content: content
+		// 게시글 로딩
+		sql =		"SELECT bno, title, writer, nickname, content "
+		sql +=		"FROM tbl_board ";
+		sql +=		"WHERE bno = '" + bno + "' ";
+		con.query(sql, function(err, result){
+			if(err)throw err;
+			console.log(result);
+	
+			title = result[0].title;
+			writer = result[0].writer;
+			nickname = result[0].nickname;
+			content = result[0].content;
+	
+			res.render('read.html',{
+				session: req.session
+				,bno: bno
+				,title: title
+				,writer: writer
+				,nickname: nickname
+				,content: content
+			});
 		});
 	});
 });
@@ -124,9 +131,8 @@ router.route('/delete').delete(function(req, res){
 	sql +=		"WHERE bno = '" + bno + "'";
 	con.query(sql, function(err, result){
 		if(err)throw err;
-		console.log(result);
+		console.log(result.message);
 
-		// 삭제 완료 alert 해야함
 		res.redirect('/');
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -57,4 +57,42 @@ router.route('/logout').post(function(req, res){
 	res.redirect('./');
 });
 
+router.route('/account').post(function(req, res){
+	if(req.body.id == "" || req.body.pw == "" || req.body.nick == ""){
+		req.flash('msg','필수 정보가 누락되어 있습니다');
+		res.redirect('/');
+		return false;
+	}
+
+	id = req.body.id;
+	pw = req.body.pw;
+	nick = req.body.nick;
+
+	var con = mysql.createConnection(dbconfig);
+	var sql = "SELECT COUNT(*) as cnt FROM tbl_login WHERE id = '" + id + "'";
+	con.query(sql, function(err, result){
+		if(err)throw err;
+		console.log(result);
+
+		if(result[0].cnt > 0){
+			req.flash('msg', '중복되는 아이디가 존재합니다');
+			res.redirect('/');
+			return false;
+		}
+
+		sql = "	INSERT INTO tbl_login(id, pw, name) VALUES('" + id + "', '" + pw + "', '" + nick + "')";
+		con.query(sql, function(err, result){
+			if(err)throw err;
+			console.log(result.message);
+		
+			//req.session.userId = id;
+			//req.session.nickname = nick;
+
+			//req.flash('session', req.session);
+			req.flash('msg', '성공적으로 계정이 생성되었습니다');
+			res.redirect('/');
+		});
+	});
+});
+
 module.exports = router;
